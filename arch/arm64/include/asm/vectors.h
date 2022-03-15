5c68b9565742269fd941990a435eed596f263ff4
James Morse <james.morse@arm.com>
2022-03-15 18:24:08 +0000

    arm64: entry: Add vectors that have the bhb mitigation sequences
    commit ba2689234be92024e5635d30fe744f4853ad97db upstream.
    
    Some CPUs affected by Spectre-BHB need a sequence of branches, or a
    firmware call to be run before any indirect branch. This needs to go
    in the vectors. No CPU needs both.
    
    While this can be patched in, it would run on all CPUs as there is a
    single set of vectors. If only one part of a big/little combination is
    affected, the unaffected CPUs have to run the mitigation too.
    
    Create extra vectors that include the sequence. Subsequent patches will
    allow affected CPUs to select this set of vectors. Later patches will
    modify the loop count to match what the CPU requires.
    
    Reviewed-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: James Morse <james.morse@arm.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/arm64/include/asm/vectors.h
+++ arch/arm64/include/asm/vectors.h
@@ -32,12 +32,6 @@ enum arm64_bp_harden_el1_vectors {
 	 * canonical vectors.
 	 */
 	EL1_VECTOR_BHB_FW,
-
-	/*
-	 * Use the ClearBHB instruction, before branching to the canonical
-	 * vectors.
-	 */
-	EL1_VECTOR_BHB_CLEAR_INSN,
 #endif /* CONFIG_MITIGATE_SPECTRE_BRANCH_HISTORY */
 
 	/*
@@ -45,29 +39,5 @@ enum arm64_bp_harden_el1_vectors {
 	 */
 	EL1_VECTOR_KPTI,
 };
-
-#ifndef CONFIG_MITIGATE_SPECTRE_BRANCH_HISTORY
-#define EL1_VECTOR_BHB_LOOP		-1
-#define EL1_VECTOR_BHB_FW		-1
-#define EL1_VECTOR_BHB_CLEAR_INSN	-1
-#endif /* !CONFIG_MITIGATE_SPECTRE_BRANCH_HISTORY */
-
-/* The vectors to use on return from EL0. e.g. to remap the kernel */
-DECLARE_PER_CPU_READ_MOSTLY(const char *, this_cpu_vector);
-
-#ifndef CONFIG_UNMAP_KERNEL_AT_EL0
-#define TRAMP_VALIAS	0
-#endif
-
-static inline const char *
-arm64_get_bp_hardening_vector(enum arm64_bp_harden_el1_vectors slot)
-{
-	if (arm64_kernel_unmapped_at_el0())
-		return (char *)TRAMP_VALIAS + SZ_2K * slot;
-
-	WARN_ON_ONCE(slot == EL1_VECTOR_KPTI);
-
-	return __bp_harden_el1_vectors + SZ_2K * slot;
-}
 
 #endif /* __ASM_VECTORS_H */
